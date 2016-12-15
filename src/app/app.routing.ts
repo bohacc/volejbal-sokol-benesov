ec9b6f68a19bd1588e6eba432a854478e632b3a7
bohac-home <bohacc@seznam.cz>
2016-12-15 11:31:05 +0100

    next

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -15,15 +15,15 @@ let title = 'Volejbal Sokol Benešov';
 const appRoutes: Routes = [
   //{ path: '', redirectTo: 'homepage', pathMatch: 'full' },
   {path: '', component: Home, data: {title: title}},
-  {path: 'home', component: Home, data: {title: title}},
+/*  {path: 'home', component: Home, data: {title: title}},
   {path: 'about-us', component: AboutUs, data: {title: title}},
   {path: 'contact', component: Contact, data: {title: title}},
   {path: 'matches', component: Matches, data: {title: title}},
   {path: 'match-schedule', component: MatchSchedule, data: {title: title}},
   {path: 'statistics', component: Statistics, data: {title: title}},
-  {path: 'players', component: Players, data: {title: title}},
+  {path: 'players', component: Players, data: {title: title}},*/
   {path: 'login', component: Login, data: {title: title}},
-  {path: '*', component: Home, data: {title: title}},
+  {path: '**', component: Home, data: {title: title}},
 ];
 
 export const appRoutingProviders: any[] = [
