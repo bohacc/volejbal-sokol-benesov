9e26ff8e3e9fb4a4c950ddf88889dc868ad764e3
bohac-home <bohacc@seznam.cz>
2016-12-24 12:07:51 +0100

    next

--- webpack.config.ts
+++ webpack.config.ts
@@ -28,8 +28,13 @@ var commonConfig = {
     new webpack.ContextReplacementPlugin(
       // The (\\|\/) piece accounts for path separators in *nix and Windows
       /angular(\\|\/)core(\\|\/)src(\\|\/)linker/,
-      root('./src/tools'),//root
+      root('/usr/share/nginx/volejbal-sokol-benesov/'),//root
+      root('/usr/share/nginx/volejbal-sokol-benesov/src/'),//root
+      root('./src/'),//root
+      root('./src/tools/'),//root
+      resolveNgRoute(root('./src')), //root
       resolveNgRoute(root('./src/tools')), //root
+      resolveNgRoute(root('./src/tools/')), //root
     ),
     new webpack.ProvidePlugin({
       $: "jquery",
