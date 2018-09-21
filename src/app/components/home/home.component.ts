e7cb0fc2f406eab0b7b8ec17a674eef7255a5f96
Martin Boháč <bohacc@seznam.cz>
2018-09-21 22:28:50 +0200

    next

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+enum SeasonsEnum {
+  SEASON2016_2017 = 20162017,
+  SEASON2017_2018 = 20172018
+}
+
 @Component({
   selector: 'home',
   templateUrl: 'home.template.html'
@@ -7,8 +12,22 @@ import { Component } from '@angular/core';
 
 export class Home {
   news: Array<Boolean> = [false, false, false];
+  season2016_2017: boolean = false;
+  season2017_2018: boolean = false;
+  seasonsEnum = SeasonsEnum;
 
   showNewsAllItem(index) {
     this.news[index] = true;
+  }
+
+  toggleSeason(season: number) {
+    switch (season) {
+      case this.seasonsEnum.SEASON2016_2017:
+        this.season2016_2017 = !this.season2016_2017;
+        break;
+      case this.seasonsEnum.SEASON2017_2018:
+        this.season2017_2018 = !this.season2017_2018;
+        break;
+    }
   }
 }
